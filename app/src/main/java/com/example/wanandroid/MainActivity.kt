10000b4ab110be78a46a70cfa2effa49f3d5ed20
zyyymn <1406401430@qq.com>
2023-09-17 15:46:34 +0800

    add app/src/java

--- app/src/main/java/com/example/wanandroid/MainActivity.kt
+++ app/src/main/java/com/example/wanandroid/MainActivity.kt
@@ -2,10 +2,95 @@ package com.example.wanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import base.BaseActivity
+import com.example.wanandroid.databinding.ActivityMainBinding
+import util.Constants
+import util.KVUtil
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    lateinit var fragmentList: MutableList<Fragment>
+    var oldFragmentIndex: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
+
+    override fun getLayoutID(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun init() {
+        mBind.imgDraw.setOnClickListener {
+            mBind.dl.open()
+        }
+
+        mBind.bnv.setOnItemSelectedListener{
+           when(it.itemId){
+               R.id.f1 -> {
+                   swithFragment(0)
+                   mBind.fName = "首页"
+                   return@setOnItemSelectedListener true
+               }
+
+               R.id.f2 ->{
+                   swithFragment(1)
+                   mBind.fName = "项目"
+                   return@setOnItemSelectedListener true
+               }
+
+               R.id.f3 ->{
+                   swithFragment(2)
+                   mBind.fName = "导航"
+                   return@setOnItemSelectedListener true
+               }
+
+               R.id.f4 ->{
+                   swithFragment(3)
+                   mBind.fName = "收藏"
+                   return@setOnItemSelectedListener true
+               }
+           }
+            false
+        }
+
+        fragmentList = mutableListOf(
+
+        )
+
+        swithFragment(0)
+        mBind.fName = "首页"
+    }
+
+
+    //切换页面
+    fun swithFragment(position: Int){
+        val targetFragment = fragmentList[position]
+        val oldFragment = fragmentList[oldFragmentIndex]
+        oldFragmentIndex = position
+        val ft = supportFragmentManager.beginTransaction()
+
+        if (oldFragment.isAdded){
+            ft.hide(oldFragment)
+        }
+
+        if (!targetFragment.isAdded){
+            ft.add(R.id.f_content,targetFragment)
+        }
+
+        ft.show(targetFragment).commitAllowingStateLoss()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val name = KVUtil.getString(Constants.USER_NAME)
+        if (name != null){
+            findViewById<TextView>(R.id.tx_info).text = name
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/wanandroid/MyApp.kt
@@ -0,0 +1,51 @@
+package com.example.wanandroid
+
+import android.app.Application
+import android.content.Context
+import androidx.viewbinding.BuildConfig
+import com.alibaba.android.arouter.launcher.ARouter
+import util.AppLogUtil
+import util.ToastUtil
+import com.tencent.mmkv.MMKV
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApp: Application() {
+
+    private val modules = mutableListOf(homeModule, ProjectModule, naviModule, meModule, userModule)
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        ToastUtil.init(this)
+        initKoin()
+        initARouter()
+        initMMKV()
+        AppLogUtil.init(this)
+    }
+
+    //初始化映射
+    private fun initMMKV() {
+        MMKV.initialize(this)
+    }
+
+    private fun initARouter() {
+        if (BuildConfig.DEBUG){
+            ARouter.openLog()
+            ARouter.openDebug()
+        }
+        ARouter.init(this)
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApp)
+            modules(modules)
+        }
+    }
+}
